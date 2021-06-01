109aa00efb97f733794565c84caf4f6f1ef51bcc
Murad Sarker <sarker24@gmail.com>
2021-06-01 21:04:46 +0200

    Action creator

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,29 @@
+import { ActionType } from "../action-types";
+import { Action } from "../Actions";
+
+interface RepositoriesState {
+    loading: boolean;
+    error: string | null;
+    data: string[];
+  }
+  
+  
+  const reducer = (
+    state: RepositoriesState,
+    action: Action
+      
+  ): RepositoriesState => {
+    switch (action.type) {
+      case ActionType.SEARCH_REPOSITORIES:
+        return { loading: true, error: null, data: [] };
+      case  ActionType.SEARCH_REPOSITORIES_SUCCESS:
+        // 100% certain that 'action' is SearchRepositoriesSuccessAction
+        return { loading: false, error: null, data: action.payload };
+      case ActionType.SEARCH_REPOSITORIES_ERROR:
+        return { loading: false, error: action.payload, data: [] };
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
